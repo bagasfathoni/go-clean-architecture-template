4ad5023f76a2d8ccb62fba285954ec8eb045e3ab
Bagas Adi Fathoni <bagasadev@outlook.com>
2022-11-26 17:14:46 +0700

    [FIX] docs & example styling for all method

--- repository/A_repository.go
+++ repository/A_repository.go
@@ -1,14 +1,13 @@
+/*
+Repository store basic methods that will directly hit to the database.
+In this github branch we create basic CRUD methods using GORM
+*/
 package repository
 
 import (
 	"github.com/bagasfathoni/go-clean-architecture-template/model"
 	"gorm.io/gorm"
 )
-
-/*
-	Repository store basic methods that will directly hit to the database.
-	In this github branch we create basic CRUD methods using GORM
-*/
 
 type aRepository struct {
 	db *gorm.DB
@@ -32,9 +31,10 @@ func (a *aRepository) Create(transferOrder *model.A) error {
 }
 
 /*
-	Find a record by using the row name and the data.
+Find a record by using the row name and the data.
 
-Example: "FindBy(map[string]interface{}{"id":1})
+	Example:
+	FindBy(map[string]interface{}{"id":1}) // will get result with ID = 1
 */
 func (a *aRepository) FindBy(by map[string]interface{}) (model.A, error) {
 	var res model.A
@@ -44,11 +44,11 @@ func (a *aRepository) FindBy(by map[string]interface{}) (model.A, error) {
 }
 
 /*
-	Find any records by using the row name and the data.
+Find any records by using the row name and the data.
 
-Example:
-"FindAllBy(map[string]interface{}{}, "created_at desc")"
-"FindAllBy(map[string]interface{}{"name:"XXX"}, "created_at desc") will get all result with name "XXX"
+	Example:
+	FindAllBy(map[string]interface{}{}, "created_at desc") // will get all result in the table
+	FindAllBy(map[string]interface{}{"name":"XXX"}, "created_at desc") // will get all result with name = "XXX"
 */
 func (a *aRepository) FindAllBy(by map[string]interface{}, orderBy string) ([]model.A, error) {
 	var res []model.A
@@ -58,12 +58,12 @@ func (a *aRepository) FindAllBy(by map[string]interface{}, orderBy string) ([]mo
 }
 
 /*
-	Find any records by using the row name and the data. This will limit the result to specific number.
+Find any records by using the row name and the data. This will limit the result to specific number.
 
-Example:
-"FindAllByWithPagination(map[string]interface{}{}, 1, 10, "created_at desc") will get the first 10 result in the column
-"FindAllByWithPagination(map[string]interface{}{}, 2, 10, "created_at desc") will get the next 10 result in the column
-"FindAllByWithPagination(map[string]interface{}{"name:"XXX"}, 2, 10, "created_at desc") will get the first 10 result in the column with name "XXX"
+	Example:
+	FindAllByWithPagination(map[string]interface{}{}, 1, 10, "created_at desc") // will get the first 10 result in the column
+	FindAllByWithPagination(map[string]interface{}{}, 2, 10, "created_at desc") // will get the next 10 result in the column
+	FindAllByWithPagination(map[string]interface{}{"name":"XXX"}, 2, 10, "created_at desc") // will get the first 10 result in the column with name = "XXX"
 */
 func (a *aRepository) FindAllByWithPagination(by map[string]interface{}, page, itemPerPage int, orderBy string) ([]model.A, error) {
 	var res []model.A
@@ -73,10 +73,10 @@ func (a *aRepository) FindAllByWithPagination(by map[string]interface{}, page, i
 }
 
 /*
-	Find any records by using any SQL Query.
+Find any records by using custom SQL Query.
 
-Example:
-"FindAllUsingCustomQuery("name = 'XXX' AND email == 'YYY', "created_at desc") will get all result in the column with name = "XXX" and email = "YYY"
+	Example:
+	FindAllUsingCustomQuery("name = 'XXX' AND email == 'YYY'", "created_at desc") // will get all result in the column with name = "XXX" and email = "YYY"
 */
 func (a *aRepository) FindAllUsingCustomQuery(query, orderBy string) ([]model.A, error) {
 	var res []model.A
@@ -85,11 +85,11 @@ func (a *aRepository) FindAllUsingCustomQuery(query, orderBy string) ([]model.A,
 }
 
 /*
-	Find any records by using any SQL Query. This will limit the result to specific number.
+Find any records by using any SQL Query. This will limit the result to specific number.
 
-Example:
-"FindAllUsingCustomQuery("name = 'XXX' AND email == 'YYY', "created_at desc", 1, 10) will get the first 10 result in the column with name = "XXX" and email = "YYY"
-"FindAllUsingCustomQuery("name = 'XXX' AND email == 'YYY', "created_at desc", 2, 10) will get the next 10 result in the column with name = "XXX" and email = "YYY"
+	Example:
+	FindAllUsingCustomQuery("name = 'XXX' AND email = 'YYY'", "created_at desc", 1, 10) // will get the first 10 result in the column with name = "XXX" and email = "YYY"
+	FindAllUsingCustomQuery("name = 'XXX' AND email = 'YYY'", "created_at desc", 2, 10) // will get the next 10 result in the column with name = "XXX" and email = "YYY"
 */
 func (a *aRepository) FindAllUsingCustomQueryWithPagination(query, orderBy string, page, itemPerPage int) ([]model.A, error) {
 	var res []model.A
@@ -98,20 +98,20 @@ func (a *aRepository) FindAllUsingCustomQueryWithPagination(query, orderBy strin
 }
 
 /*
-	Update a value in a record.
+Update a value in a record.
 
-Example:
-"UpdateBy(map[string]interface{}{"id":1}, map[string]interface{}{"name":"YYY"}) will update a record name with ID = 1 to "YYY"
+	Example:
+	UpdateBy(map[string]interface{}{"id":1}, map[string]interface{}{"name":"YYY"}) // will update a record name with ID = 1 to "YYY"
 */
 func (a *aRepository) UpdateBy(by map[string]interface{}, value map[string]interface{}) error {
 	return a.db.Model(model.A{}).Where(by).Updates(value).Error
 }
 
 /*
-	Delete a record.
+Delete a record.
 
-Example:
-"DeleteBy(map[string]interface{}{"id":1}) will delete a record name with ID = 1
+	Example:
+	DeleteBy(map[string]interface{}{"id":1}) // will delete a record name with ID = 1
 */
 func (a *aRepository) DeleteBy(by map[string]interface{}) error {
 	return a.db.Model(model.A{}).Delete(by).Error
