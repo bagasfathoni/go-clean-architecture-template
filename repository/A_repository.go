85697a5c0ad3214f1b9a8387105e090b3e157faa
Bagas Adi Fathoni <bagasadev@outlook.com>
2022-11-26 15:06:15 +0700

    [ADD] basic CRUD methods to repository

--- repository/A_repository.go
+++ repository/A_repository.go
@@ -1,21 +1,77 @@
 package repository
 
-import "github.com/bagasfathoni/go-clean-architecture-template/model"
+import (
+	"github.com/bagasfathoni/go-clean-architecture-template/model"
+	"gorm.io/gorm"
+)
+
+/*
+	Repository store basic methods that will directly hit to the database.
+	In this github branch we create basic CRUD methods using GORM
+*/
 
 type aRepository struct {
-	repo model.A
+	db *gorm.DB
 }
 
 type ARepository interface {
-	Foo()
+	Create(newA *model.A) error
+	UpdateBy(by map[string]interface{}, value map[string]interface{}) error
+	FindBy(by map[string]interface{}) (model.A, error)
+	FindAllBy(by map[string]interface{}, orderBy string) ([]model.A, error)
+	FindAllByWithPagination(by map[string]interface{}, page, itemPerPage int, orderBy string) ([]model.A, error)
+	FindAllUsingCustomQuery(query, orderBy string) ([]model.A, error)
+	FindAllUsingCustomQueryWithPagination(query, orderBy string, page, itemPerPage int) ([]model.A, error)
+	Delete(A *model.A) error
 }
 
-func (a *aRepository) Foo() {
-	panic("needs implementation")
+func (a *aRepository) Create(transferOrder *model.A) error {
+	result := a.db.Create(&transferOrder).Error
+	return result
 }
 
-func InitARepository(a model.A) ARepository {
-	aRepo := new(aRepository)
-	aRepo.repo = a
-	return aRepo
+func (a *aRepository) FindBy(by map[string]interface{}) (model.A, error) {
+	var res model.A
+	err := a.db.Where(by).First(&res).Error
+	return res, err
+
+}
+
+func (a *aRepository) FindAllByWithPagination(by map[string]interface{}, page, itemPerPage int, orderBy string) ([]model.A, error) {
+	var res []model.A
+	err := a.db.Where(by).Order(orderBy).Limit(itemPerPage).Offset(page).Find(&res).Error
+	return res, err
+
+}
+
+func (a *aRepository) FindAllBy(by map[string]interface{}, orderBy string) ([]model.A, error) {
+	var res []model.A
+	err := a.db.Where(by).Order(orderBy).Unscoped().Find(&res).Error
+	return res, err
+
+}
+
+func (a *aRepository) FindAllUsingCustomQuery(query, orderBy string) ([]model.A, error) {
+	var res []model.A
+	err := a.db.Where(query).Order(orderBy).Unscoped().Find(&res).Error
+	return res, err
+}
+
+func (a *aRepository) FindAllUsingCustomQueryWithPagination(query, orderBy string, page, itemPerPage int) ([]model.A, error) {
+	var res []model.A
+	err := a.db.Where(query).Order(orderBy).Limit(itemPerPage).Offset(page).Find(&res).Error
+	return res, err
+}
+
+func (a *aRepository) UpdateBy(by map[string]interface{}, value map[string]interface{}) error {
+	return a.db.Model(model.A{}).Where(by).Updates(value).Error
+}
+
+func (a *aRepository) Delete(A *model.A) error {
+	res := a.db.Delete(A).Error
+	return res
+}
+
+func InitARepository(a *gorm.DB) ARepository {
+	return &aRepository{db: a}
 }
